6da7a391f7d615cc7c019a0a20bc8a8c64445a77
boomNDS <boomsixboom@gmail.com>
2021-02-11 12:54:09 +0700

    add plugin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Dashboard from './components/Dashboard'
+import UserDisplay from './components/UserDisplay'
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link,
+  // useRouteMatch,
+  // useParams
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/user_display">
+            <UserDisplay />
+          </Route>
+          <Route path="/">
+            <Dashboard name="good" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
---
+++ src/components/UserDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function UserDisplay(props) {
+    return <h1>UserDisplay, {props.name}</h1>;
+}
+
+export default UserDisplay
